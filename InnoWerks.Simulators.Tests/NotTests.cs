602af9ca63bde0aa9bd82bcff2524108a042e100
Michaeljon Miller <michaeljon.miller@outlook.com>
2024-09-12 19:17:41 -0700

    stash

--- InnoWerks.Simulators.Tests/NotTests.cs
+++ InnoWerks.Simulators.Tests/NotTests.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using InnoWerks.Assemblers;
 using InnoWerks.Processors;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -86,6 +88,38 @@ namespace InnoWerks.Simulators.Tests
                 TestContext.Write("|");
             }
             TestContext.WriteLine("\r");
+        }
+
+        [TestMethod]
+        public void RegexSwitch()
+        {
+            var commandRegex = new Regex(
+                "^((?<command>t) (?<steps>[0-9]+))$?|" +
+                "^((?<command>pc) (?<addr>[a-f0-9]{4}))$?|" +
+                "^((?<command>jsr) (?<addr>[a-f0-9]{4}))$?|" +
+                "^(?<command>sb (?<addr>[a-f0-9a-f]{4}))$?|" +
+                "^(?<command>cb (?<addr>[a-f0-9]{4}))$?|" +
+                "^(?<command>ca)$?|" +
+                "^(?<command>lb)$?|" +
+                "^(?<command>df)$?|" +
+                "^(?<command>sf (?<flag>[cnvz]))$?|" +
+                "^(?<command>cf (?<flag>[cnvz]))$?|" +
+                "^(?<command>dr)$?|" +
+                "^(?<command>sr (?<register>[axys]) (?<value>[a-f0-9]{1,2}))$?|" +
+                "^(?<command>zr (?<register>[axys]))$?|" +
+                "^(?<command>w (?<addr>[a-f0-9]{1,4}) (?<values>[a-f0-9]{1,2}( [a-f0-9]{1,2})*))$?|" +
+                "^(?<command>r (?<addr>[a-f0-9]{1,4}) (?<len>[0-9]*))$?|" +
+                "^(?<command>d (?<page>[a-f0-9]{1,2}))$?|" +
+                "^(?<command>o ts (?<speed>[0-9]+))?|$" +
+                "^(?<command>o tv (?<flag>(true|false)))$?|" +
+                "^(?<command>(q|quit))$?|" +
+                "^(?<command>g)$?|" +
+                "^(?<command>s)$?|" +
+                "^(?<command>(\\?|h))$?"
+            );
+
+            commandRegex.IsMatch("t 10");
+            commandRegex.MatchNamedCaptures("t 10");
         }
     }
 }
